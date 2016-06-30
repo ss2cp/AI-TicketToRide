2b088b08058f42dd7c9440e17dd49d4a52d1e530
Shao Sun <ss2cp@virginia.edu>
2016-06-30 02:09:05 -0400

    Update for the night of Jun. 29
    // TODO starts coding from here
    // TODO buy ticket routes first
    // TODO write another find shortest path method to deal with claimed
    routes
    // TODO check possibility and affordability of each owned ticket
    // TODO See if it is ok to
    return a random route if two routes exist between two cities
    
    Summary:
    1) Wrote a separate method for sort ArrayList<Routes> in descending
    order by their costs.
    2) Created getNeighbors(Destination city) to return all the neighbors
    of a given city.
    3) Created shortestPath(Destination from, Destination to) to return a
    stack of the shortest path between two cities.
    4) Created printRoute(HashMap<Destination, Destination> parent,
    Destination child, Stack<Route> rt) to recursively push routes of the
    child and parent city to the stack
    5) Created getOneWayRoute(Destination from, Destination to) to return
    the route objects between the given cities if it exists (returns null
    if not exist)

--- src/ttr/model/player/ShaoPlayer.java
+++ src/ttr/model/player/ShaoPlayer.java
@@ -2,7 +2,13 @@ package ttr.model.player;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+import com.sun.corba.se.impl.orbutil.graph.Node;
 
 import ttr.model.destinationCards.Destination;
 import ttr.model.destinationCards.DestinationTicket;
@@ -30,32 +36,27 @@ public class ShaoPlayer extends Player {
 	 * */
 	@Override
 	public void makeMove() {
+		// return a stack of the shortest path from Dest to Dest
+		shortestPath(Destination.Toronto, Destination.Charleston);
+		// TODO starts coding from here
+		// TODO buy ticket routes first
+		// TODO write another find shortest path method to deal with claimed
+		// routes
+
+		// Store all affordable routes in the ArrayList
 		ArrayList<Route> possRoutes = possibleRoutes();
 
-		// sort the list in ascending order of cost
-		((List<Route>) possRoutes).sort((o1, o2) -> Integer.compare(
-				o1.getCost(), o2.getCost()));
-		// reverse the list so that the larger cost will be bought first
-		Collections.reverse(possRoutes);
+		// sort the list in descending order of cost
+		possRoutes = sortDescen(possRoutes);
 
 		/**
 		 * test and see if any destination ticket is affordable
 		 */
 		// iterate through the tickets
 		for (DestinationTicket ticket : super.getDestinationTickets()) {
-			// iterate through the affordable routes
-			for (Route route : possRoutes) {
-				// if destination ticket is an affordable route
-				if ((route.getDest1().equals(ticket.getFrom())
-						&& route.getDest2().equals(ticket.getTo()) || (route
-						.getDest2().equals(ticket.getFrom()) && route
-						.getDest1().equals(ticket.getTo())))) {
-					// TODO need to write a method to get the passing route
-					// between two cities on the ticket
-
-				}
-			}
+			// TODO check possibility and affordability of each owned ticket
 		}
+
 		if (!possRoutes.isEmpty()) {
 			// System.out.println("Player has "
 			// + super.getNumTrainCardsByColor(possRoutes.get(0)
@@ -76,11 +77,9 @@ public class ShaoPlayer extends Player {
 
 		/*
 		 * Something like this will allow an AI to attempt to buy a route on the
-		 * board. The first param is the route they wish
-		 */
-		/*
-		 * ...to buy, the second param is the card color they wish to pay for
-		 * the route with (some routes have options here)
+		 * board. The first param is the route they wish to buy, the second
+		 * param is the card color they wish to pay for the route with (some
+		 * routes have options here)
 		 */
 		super.claimRoute(new Route(Destination.Atlanta, Destination.Miami, 6,
 				TrainCardColor.blue), TrainCardColor.blue);
@@ -90,6 +89,129 @@ public class ShaoPlayer extends Player {
 		 * of these things in one turn. The simulator won't allow it
 		 */
 
+	}
+
+	/**
+	 * Returns all the neighbors of a given city
+	 * */
+	public ArrayList<Destination> getNeighbors(Destination city) {
+		ArrayList<Destination> toReturn = new ArrayList<Destination>();
+
+		for (Route route : Routes.getInstance().getAllRoutes()) {
+			if (route.getDest1() == city
+					&& !toReturn.contains(route.getDest2()))
+				toReturn.add(route.getDest2());
+
+			if (route.getDest2() == city
+					&& !toReturn.contains(route.getDest1()))
+				toReturn.add(route.getDest1());
+		}
+
+		return toReturn;
+	}
+
+	/**
+	 * Returns a stack of the shortest path between two cities.
+	 * */
+	public Stack<Route> shortestPath(Destination from, Destination to) {
+		Stack<Route> rt = new Stack<Route>();
+
+		/* If same, just return an empty stack */
+		if (from == to)
+			return rt;
+
+		/* Open and Closed lists (breadth first search) */
+		HashMap<Destination, Integer> openList = new HashMap<Destination, Integer>();
+		HashMap<Destination, Integer> closedList = new HashMap<Destination, Integer>();
+		// initiate a hashmap to store each node's parent, key is the child,
+		// object is the parent
+		HashMap<Destination, Destination> parent = new HashMap<Destination, Destination>();
+
+		openList.put(from, 0);
+
+		while (openList.size() > 0) {
+
+			/* Pop something off the open list, if destination then return true */
+			Destination next = null;
+			int minCost = 9999;
+			for (Destination key : openList.keySet()) {
+				if (openList.get(key) < minCost) {
+					next = key;
+					minCost = openList.get(key);
+				}
+
+			}
+
+			/* Take it off the open list and put on the closed list */
+			openList.remove(next);
+			closedList.put(next, minCost);
+
+			/* If this is the destination, then return!!!! */
+			if (next == to) {
+				// System.out.println("test "+closedList);
+				break;
+			}
+
+			/*
+			 * Get all the neighbors of the next city that aren't on open or
+			 * closed lists already
+			 */
+			for (Destination neighbor : getNeighbors(next)) {
+				if (closedList.containsKey(neighbor))
+					continue;
+
+				/*
+				 * get route between next and neighbor and see if better than
+				 * neighbor's value
+				 */
+				ArrayList<Route> routesToNeighbor = this.getRoutes(next,
+						neighbor);
+				for (Route routeToNeighbor : routesToNeighbor) {
+					int newCost = closedList.get(next)
+							+ routeToNeighbor.getCost();
+
+					if (openList.containsKey(neighbor)) {
+						if (newCost < openList.get(neighbor)) {
+							parent.put(neighbor, next);
+							openList.put(neighbor, newCost);
+						}
+					} else {
+						openList.put(neighbor, newCost);
+						parent.put(neighbor, next);
+					}
+				}
+			}
+		}
+
+		// push the last city to destination to the stack
+		rt.push(getOneWayRoute(parent.get(to), to));
+		// recursively push route to the stack until the starting city
+		return printRoute(parent, parent.get(to), rt);
+	}
+
+	/*
+	 * A recursive method to push routes of the child and parent city to the
+	 * stack
+	 */
+	public Stack<Route> printRoute(HashMap<Destination, Destination> parent,
+			Destination child, Stack<Route> rt) {
+		if (parent.get(child) == null) {
+			return rt;
+		}
+		rt.push(getOneWayRoute(parent.get(child), child));
+		return printRoute(parent, parent.get(child), rt);
+	}
+
+	/*
+	 * Sort the given ArrayList of Route to descending order of cost
+	 */
+	public ArrayList<Route> sortDescen(ArrayList<Route> possRoutes) {
+		// sort the list in ascending order of cost
+		((List<Route>) possRoutes).sort((o1, o2) -> Integer.compare(
+				o1.getCost(), o2.getCost()));
+		// reverse the list so that the larger cost will be bought first
+		Collections.reverse(possRoutes);
+		return possRoutes;
 	}
 
 	/*
@@ -109,7 +231,7 @@ public class ShaoPlayer extends Player {
 				TrainCardColor color = nextRoute.getColor();
 				int cost = nextRoute.getCost();
 				// if player has more than the required cost of such color,
-				// add to return arraylist
+				// add to return ArrayList
 				if (super.getNumTrainCardsByColor(color) >= cost) {
 					// System.out.println("color is "+color+
 					// ", cost is "+cost+". Player currently has "+super.getNumTrainCardsByColor(color)+" "+color+" train cards.");
@@ -153,6 +275,21 @@ public class ShaoPlayer extends Player {
 			}
 		}
 		return toReturn;
+	}
+
+	/**
+	 * Returns the route objects between the given cities if it exists (returns
+	 * null if not exist)
+	 * Return a random one if two routes exist TODO see if this is ok
+	 * */
+	public Route getOneWayRoute(Destination from, Destination to) {
+
+		for (Route route : Routes.getInstance().getAllRoutes()) {
+			if (route.getDest1() == from && route.getDest2() == to) {
+				return route;
+			}
+		}
+		return null;
 	}
 
 }
